98153c624c44d44f13ec2627d7af6161b190eb68
diego <diegocornejo958@gmail.com>
2025-05-15 19:58:12 -0600

    Allow origins

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,8 @@ import cors from 'cors';
 const AVAILABLES = [
   'http://localhost:3000',
   'http://localhost:3001',
-  'http://127.0.0.1:5500'
+  'http://127.0.0.1:5500',
+  '*'
 ]
 
 export const corsSchema = ({acceptOrigins = AVAILABLES} = {}) => cors({
